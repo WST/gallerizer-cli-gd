965d07130279784ecc190a4d2df7a4927468118d
WST <admin@jsmart.web.id>
2014-03-19 02:06:51 +0400

    Better little than nothing…

--- src/image.cpp
+++ src/image.cpp
@@ -1,10 +1,16 @@
 
 #include "image.h"
 
-Image::Image() {
+Image::Image(std::string filename) {
+	FILE *file = fopen(filename.c_str(), "r");
+	image = gdImageCreateFromJpeg(file);
+	fclose(file);
+}
+
+Image::Image(const Image &image) {
 	
 }
 
 Image::~Image() {
-	
+	gdImageDestroy(image);
 }
--- src/include/image.h
+++ src/include/image.h
@@ -2,11 +2,18 @@
 #ifndef IMAGE_H
 #define IMAGE_H
 
+#include <gd.h>
+#include <string>
+
 class Image
 {
 	public:
-		Image();
+		Image(std::string filename);
+		Image(const Image &image);
 		~Image();
+
+	private:
+		gdImagePtr image;
 };
 
 #endif
